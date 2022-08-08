bcbcbe77063a94a7df0c699797fa8de3d622d295
buudee625 <liu.andrew625@gmail.com>
2022-08-08 01:52:06 -0700

    formats the index table and date format

--- controllers/flight.js
+++ controllers/flight.js
@@ -8,8 +8,6 @@ module.exports = {
 
 function index(req, res) {
     Flight.find({}, function(err, flightDoc){
-        console.log(flightDoc);
-
         res.render('flights/index.ejs', {
             flights: flightDoc
         })
@@ -22,12 +20,14 @@ function newFlight(req, res) {
 
 function create(req, res) {
     console.log(req.body, '<-- req.body')
+    req.body.departs = req.body.departs.substring(0, 10);
+    console.log(req.body.departs, '<-- req.body.departs')
     Flight.create(req.body, function(err, flightsDoc){
         if(err){
             console.log(err, '<-- err in flight create func')
             return res.render('flights/new.ejs')
         };
-        console.log(flightsDoc);
+        // console.log(flightsDoc, '<--flightDoc');
         res.redirect('/flights')
     })
 };
